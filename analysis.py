3b531c3ebe075e361c786ccca8479ef19b75dd79
JairdanC <jairdanchopra@gmail.com>
2026-02-22 13:20:17 -0500

    Data processing completed, this is the version used in lab, for public display add explanatory comments, directories and README

--- analysis.py
+++ analysis.py
@@ -12,16 +12,23 @@ def create_logy_plot(df_x, df_y):
     y = df_y.to_numpy()
 
     log_y = np.log(y)
-    slope, b = np.polyfit(x, log_y, 1) #Fit a polynomial of degree 1 (linear), tau is slope, b is y-intercept
+    slope, b = np.polyfit(x, log_y, 1) #Fit a polynomial of degree 1 (linear), slope is slope, b is y-intercept
     y_theory = slope*x + b
     y_err = (y*0.015 + 0.01) * (y**(-1)) #dy/y
 
     fig, ax = plt.subplots()
     plt.errorbar(x, log_y, yerr=y_err, fmt='o', color='darkblue', label='Experimental Data')
-    plt.plot(x, y_theory, linewidth=2.0, color='orange', label='Line of Best Fit')
-    ax.grid(True, linestyle=':', alpha=0.4)
+    plt.plot(x, y_theory, linewidth=2.0, linestyle='--', color='red', label='Line of Best Fit')
+    ax.grid(True, linestyle=':', alpha=0.6)
+    ax.legend()
+    ax.set_ylabel('ln(Voltage [Volts])')
+    ax.set_xlabel(r'Time [$\mu$s]')
+
+    #rescale tau to seconds
+    slope = slope/1e-6
     plt.savefig('decay_log_plot.png')
-    print("Tau: "  + str(-1/slope))
+    print("Experimental Tau: "  + str(-1/slope))
+    print("Theoretical Tau: " + str(R*C))
     print("FIGURE SAVED")
 
 #Task 3/4
@@ -29,6 +36,7 @@ def create_bode_plot(sig_in, sig_out_df, freq_df, type):
     #sig_in is constant
     sig_out = sig_out_df.to_numpy()
     freq = freq_df.to_numpy()
+    bp_intersect = np.ones(freq.size) * -3
 
     gain = 20 * (np.log10(sig_out/sig_in))
     log_freq = np.log10(freq)
@@ -40,8 +48,21 @@ def create_bode_plot(sig_in, sig_out_df, freq_df, type):
 
     fig, ax = plt.subplots()
     plt.errorbar(log_freq, gain, yerr=gain_err, fmt='o', color='darkblue', label='Experimental Data')
-    plt.plot(log_freq, gain_theory, linewidth=2.0, color='orange', label='Line of Best Fit')
-    ax.grid(True, linestyle=':', alpha=0.4)
+    plt.plot(log_freq, gain_theory, linewidth=2.0, linestyle='--', color='red', label='Line of Best Fit')
+    plt.plot(log_freq, bp_intersect, linewidth=2.0, linestyle='--', color='grey', label='Breakpoint Gain [-3dB]')
+    ax.grid(True, linestyle=':', alpha=0.6)
+    ax.legend()
+    ax.set_ylabel('Gain [dB]')
+    ax.set_xlabel('Log10(Frequency [Hz])')
+
+    print("Theoretical Half Power Frequency: " + str(1/(2*np.pi*R*C)))
+    freq_interp = np.arange(400, 550)
+    log_freq_interp = np.log10(freq_interp)
+    gain_interp = np.interp(log_freq_interp, log_freq, gain) #linear interpolation of the data to find a minimum, range used for around 70 plus/minus theoretical value
+    bp_intersect = np.ones(gain_interp.size) * -3
+    difference = np.abs(gain_interp - bp_intersect) 
+    bp_exp_freq = freq_interp[np.argmin(difference)]
+    print("Experimental Half Power Frequency: " + str(bp_exp_freq))
     plt.savefig(str(type) + '_pass_filter_bode_plot.png')
     print("FIGURE SAVED")
 
